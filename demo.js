6007c8752695acb65542967c91d4388ea2a0d245
unknown <damisusefu@gmail.com>
2022-11-10 10:21:38 +0300

    arrayOfJavascript

--- demo.js
+++ demo.js
@@ -6,5 +6,47 @@ console.log("max boqqolo");
 //loops of javascript
 for(let i=0;i<=6;i++)
 {
-    console.log('loop of javascript');
+    console.log('loop of javascript' + i);
+    if(i==3)
+    break;
 }
+const names=['john','ali','mary','bob'];
+for(let i=0;i<names.length;i++)
+{
+    console.log(names[i])
+    if(i==3)
+    break;
+}
+
+for(name of names) console.log(name);
+//objects
+const user={firstName:'John',lastName:'Doe'};
+for(key in user) console.log(user[key]);
+let i=0;
+while(i<10)
+{
+    
+    i++;
+    if(i==5)
+    break;
+    console.log(i)
+}
+const animals=['cat','dog','horse','sheep'];
+animals.forEach(animal=> {
+console.log(animal);
+})
+//arry of javascript
+const items=[
+    {name:'Bike', price:100},
+    {name:'Ali' ,price:200},
+    {name:'book', price:10},
+    { name:'computer',price:1000 },
+    {name:'phone',price:500},
+    {name:'keyboard', price:25},
+    {name:'TV', price:5}
+]
+const filteredItems=items.filter((item)=>
+{
+    return item.price <=100
+})
+console.log(filteredItems)
